f3e7fdf88e42847fa2e2e2ecb7963a2dccb82467
NABIL ALIMURA <45660685+nabilalimura@users.noreply.github.com>
2019-01-02 18:19:57 +0700

    Add files via upload

---
+++ MENGHITUNG PERSEGI.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int panjang,lebar,luas;
+
+    cout<<"program hitung luas persegi panjang\n";
+
+    cout<<"masukan panjang =";
+
+    cin>>panjang;
+
+    cout<<"masukan lebar =";
+
+    cin>>lebar;
+
+    luas =panjang * lebar;
+
+    cout<<"maka hasil luas persegi panjang="<<luas<<"\n";
+
+    ("pause");
+
+    return 0;
+}
